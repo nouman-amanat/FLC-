83f4e885f8574ab081bd70b5846384669c1969ba
nouman-amanat <105341801+nouman-amanat@users.noreply.github.com>
2025-09-19 20:42:36 +0500

    uploaded all file

---
+++ index.js
@@ -0,0 +1,22 @@
+let x = 2;
+let y = 4;
+let z = x+y;
+
+
+let a= x-y;
+console.log(z);
+console.log(a);
+
+let b ="My name is Abdullah. I am basically from lakki marwat, kpk";
+let int= 123;
+let char = 'a';
+let bool= true;
+
+console.log(typeof(b));
+console.log(typeof(int));
+console.log(typeof(char));
+console.log(typeof(bool));
+
+
+
+
